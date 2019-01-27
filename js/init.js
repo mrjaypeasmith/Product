fe11f804f0fdb1c8b318a354fb047850f4563e4c
Jay <jermainepaulsmith@icloud.com>
2019-01-27 16:42:33 +0000

    First Commit

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    
+    $('.button-collapse').sideNav();
+    $('.tooltipped').tooltip({delay: 50});
+         
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
